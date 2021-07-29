b42518fcbbd0f151a710b2407e0a951ad38449b3
Eakkawat Louhawattanavanich <eakkawatlouhawattanavanich@MacBook-Pro-khxng-Eakkawat.local>
2021-07-29 21:03:31 +0700

    first Commit

---
+++ front/src/components/ExpenseApp.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-app-bar color="primary">
+      <v-spacer></v-spacer>
+      <v-app-bar-title>Vuetify</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <!-- <v-btn text>Developer</v-btn> -->
+
+      <!-- <v-btn class="mr-3" text>Help</v-btn>
+
+      <v-divider inset vertical></v-divider>
+
+      <v-btn class="mr-4 ml-5" color="primary" plain>
+        <v-icon left icon="mdi-handshake-outline"></v-icon>
+
+        <span>John Leider</span>
+      </v-btn> -->
+    </v-app-bar>
+    <v-main>
+      <v-container><h1>Expense App</h1></v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'ExpenseApp',
+};
+</script>
+
+<style></style>
---
+++ front/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ExpenseApp />
+  </div>
+</template>
+
+<script>
+import ExpenseApp from '../components/ExpenseApp.vue';
+
+export default {
+  name: 'Home',
+
+  components: {
+    ExpenseApp,
+  },
+};
+</script>
